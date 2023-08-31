1514ce6d2ec122c0c28b4ade3833c07b704dbb15
ghkd1418 <1004wipi@gmail.com>
2023-09-01 00:23:34 +0900

    test: 랜딩페이지 링크버튼 테스트 작성

--- src/test/unit/index.test.tsx
+++ src/test/unit/index.test.tsx
@@ -1,13 +1,18 @@
-import { render, screen } from '@testing-library/react'
-import LandingPage from '@/pages'
-import userEvent from '@testing-library/user-event'
+import LandingPage from '@pages/index';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe(`<LandingPage />`, () => {
-	it('chat버튼을 누르면 chat 페이지로 이동한다.', () => {
-		render(<LandingPage />)
-		const chatButton = screen.getByRole('button', { name: 'chat' })
-		userEvent.click(chatButton)
+	it('랜딩페이지 링크 버튼을 누르면 해당 페이지로 이동한다.', () => {
+		render(<LandingPage />);
+		const chatButton = screen.getByRole('link', { name: 'chat' });
+		userEvent.click(chatButton);
 
-		expect(global.window.location.href).toContain('/chat')
-	})
-})
+		expect(chatButton).toHaveAttribute('href', '/chat');
+
+		const voiceButton = screen.getByRole('link', { name: 'voice' });
+		userEvent.click(chatButton);
+
+		expect(voiceButton).toHaveAttribute('href', '/voice');
+	});
+});
