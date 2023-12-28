5c83b143a36558fee9e180434724c00890537b9e
tangjinlog <lkg70007@gmail.com>
2023-12-28 19:26:14 +0900

    fix: <BackButton/> pushState 로직 추가 [#17]

--- src/components/atoms/button/BackButton.tsx
+++ src/components/atoms/button/BackButton.tsx
@@ -1,5 +1,6 @@
 import Button from '@atoms/button/Button';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 interface PropTypes extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
@@ -7,6 +8,10 @@ interface PropTypes extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function BackButton({ children, ...props }: PropTypes) {
 	const router = useRouter();
+	useEffect(() => {
+		window.history.pushState(null, '', router.asPath);
+	}, []);
+
 	return (
 		<Button onClick={() => router.back()} {...props}>
 			{children}
