ec1460a50266946769ee5b0c4dd23db70f453631
ghkd1418 <1004wipi@gmail.com>
2023-12-28 17:35:48 +0900

    refacotr: Login 컴포넌트 분리 [#3]

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,16 +1,12 @@
 import Logo from '@atoms/logo/Logo';
-import { IconProfile } from '@svgs/index';
-import { KAKAO_LOGIN_URI } from '@utils/variables';
+import Login from '@molecules/login';
 
 function Header() {
 	return (
-		<div className="headerWrapper">
+		<header className="headerWrapper">
 			<Logo size={`mid`} />
-			<a className="loginBtn" href={KAKAO_LOGIN_URI}>
-				<IconProfile />
-				<span>로그인</span>
-			</a>
-		</div>
+			<Login />
+		</header>
 	);
 }
 
