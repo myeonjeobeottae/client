6a91a3f54d7e4eefd0ae0cf3686afe9af46408f8
ghkd1418 <1004wipi@gmail.com>
2023-12-31 14:52:49 +0900

    refactor: back버튼 분리

--- src/components/templates/layout/Layout.tsx
+++ src/components/templates/layout/Layout.tsx
@@ -1,19 +1,13 @@
 import Header from '@organisms/header/Header';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Background from '@images/landingPage/bg.png';
-import { IconBack } from '@svgs/index';
-import { BackButton } from '@atoms/button/BackButton';
+import ConditionBackButton from './ConditionBackButton';
 
 interface LayoutProps {
 	children: React.ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
-	const { query, pathname } = useRouter();
-	const isQuestion = !!query.q;
-	const viewBackBtn = pathname !== '/' && !isQuestion;
-
 	return (
 		<div className="layoutWrapper">
 			<Image
@@ -25,12 +19,8 @@ function Layout({ children }: LayoutProps) {
 				alt="Background Image"
 			/>
 			<Header />
-			{viewBackBtn ? (
-				<BackButton>
-					<IconBack />
-				</BackButton>
-			) : null}
-
+			//FIXME: ConditionBackButton 한뎁스 내리기
+			<ConditionBackButton />
 			{children}
 		</div>
 	);
