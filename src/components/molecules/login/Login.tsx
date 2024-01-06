23d642e73abcf0bfc9b2dd6c35c32dec48a94905
ghkd1418 <1004wipi@gmail.com>
2024-01-06 23:55:25 +0900

    feat: 로그인 여부에 따른 조건부 랜더링

--- src/components/molecules/login/Login.tsx
+++ src/components/molecules/login/Login.tsx
@@ -1,14 +1,7 @@
 import { IconProfile } from '@svgs/index';
 import { KAKAO_LOGIN_URI } from '@utils/variables';
-import { AuthContext } from 'context/Auth';
-import { useContext } from 'react';
 
 function Login() {
-	// const authService = useContext(AuthContext);
-
-	// const user = authService?.getCurrentUser();
-	// console.log('user', user);
-
 	return (
 		<a className="loginBtn" href={KAKAO_LOGIN_URI}>
 			<IconProfile />
--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,14 +1,20 @@
 import Logo from '@atoms/logo/Logo';
 import Login from '@molecules/login';
 import React from 'react';
+import { AuthContext } from 'context/Auth';
+import { useContext } from 'react';
+import Profile from '@molecules/profile/Profile';
 
 function Header() {
+	const authService = useContext(AuthContext);
+	const user = authService?.getCurrentUser();
+
 	return (
 		<header className="headerWrapper">
 			<Logo size={`mid`} />
-			<Login />
+			{user ? <Profile user={user} /> : <Login />}
 		</header>
 	);
 }
 
-export default React.memo(Header);
+export default Header;
