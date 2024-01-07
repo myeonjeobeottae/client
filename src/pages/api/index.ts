c02ac6b6ff746ad58a1e3212ae9735e28edb20c4
ghkd1418 <1004wipi@gmail.com>
2024-01-07 17:33:29 +0900

    feat: accessToken 만료 시 재발급 [#3]

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,9 +1,38 @@
-import Axios, { AxiosInstance } from 'axios';
+import Axios, { AxiosError, AxiosInstance } from 'axios';
+import { User } from 'context/Auth';
 
 export const createApi = (): AxiosInstance => {
 	const customAxios = Axios.create({
 		baseURL: 'https://interviewee.store',
 		withCredentials: true,
+	});
+
+	customAxios.interceptors.response.use(
+		(response) => {
+			console.log(response.data);
+			return Promise.resolve(response.data);
+		},
+
+		async (error: AxiosError) => {
+			console.log(error.response);
+			//refreshAccessToken
+			if (error.response?.status === 403) {
+				const userData = (await customAxios.get('/kakao/renew/token')) as User;
+				localStorage.setItem('__token', userData?.accessToken);
+				return;
+			}
+			return Promise.reject(error);
+		},
+	);
+
+	customAxios.interceptors.request.use((config) => {
+		const token = localStorage.getItem('__token');
+
+		if (token) {
+			config.headers['Authorization'] = `Bearer ${token}`;
+		}
+
+		return config;
 	});
 
 	return customAxios;
