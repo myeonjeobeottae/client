e521410aef18ff73cea7d4817f9a03aebf730fd5
tangjinlog <lkg70007@gmail.com>
2024-01-09 00:21:07 +0900

    fix: code

--- src/components/molecules/login/Login.tsx
+++ src/components/molecules/login/Login.tsx
@@ -4,7 +4,11 @@ import { KAKAO_LOGIN_URI } from '@utils/variables';
 function Login() {
 	console.log(process.env.NEXT_PUBLIC_REDIRECT_URI);
 	return (
-		<a className="loginBtn" href={`${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI}`}>
+		// <a className="loginBtn" href={`${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI}`}>
+		<a
+			className="loginBtn"
+			href={`kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=undefined&through_account=true&client_id=kkkkkk`}
+		>
 			<IconProfile />
 			<span>로그인</span>
 		</a>
