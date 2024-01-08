de8f655dc667320d9fca1e5850fd5bf0fd37122b
tangjinlog <lkg70007@gmail.com>
2024-01-08 23:41:28 +0900

    fix: code

--- src/components/molecules/login/Login.tsx
+++ src/components/molecules/login/Login.tsx
@@ -2,6 +2,7 @@ import { IconProfile } from '@svgs/index';
 import { KAKAO_LOGIN_URI } from '@utils/variables';
 
 function Login() {
+	console.log(process.env.NEXT_PUBLIC_REDIRECT_URI);
 	return (
 		<a className="loginBtn" href={KAKAO_LOGIN_URI}>
 			<IconProfile />
