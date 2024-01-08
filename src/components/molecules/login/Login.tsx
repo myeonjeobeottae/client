ad1251734735038f25caa04181c21e1b359c9761
tangjinlog <lkg70007@gmail.com>
2024-01-09 01:46:46 +0900

    fix: 해치웠나

--- src/components/molecules/login/Login.tsx
+++ src/components/molecules/login/Login.tsx
@@ -1,8 +1,6 @@
 import { IconProfile } from '@svgs/index';
-import { KAKAO_LOGIN_URI } from '@utils/variables';
 
 function Login() {
-	console.log(process.env.NEXT_PUBLIC_REDIRECT_URI);
 	return (
 		<a className="loginBtn" href={`${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI}`}>
 			<IconProfile />
--- src/components/templates/generationTemp/CustomFunnelTemp.tsx
+++ src/components/templates/generationTemp/CustomFunnelTemp.tsx
@@ -23,7 +23,6 @@ function CustomFunnelTemp() {
 				<Modal>
 					<Modal.Overlay />
 					<Modal.Title>라우터 감지 모달</Modal.Title>
-					<Modal.Title>{`${process.env.NEXT_PUBLIC_REST_API_KEY}`}</Modal.Title>
 					<Modal.CancelButton>취소</Modal.CancelButton>
 					<Modal.ExecuteButton unBlockingWithCallback={unBlockingWithCallback}>
 						나가기
