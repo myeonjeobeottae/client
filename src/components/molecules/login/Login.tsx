88b820db9c5702948d38ee1208679c3d624707f8
tangjinlog <lkg70007@gmail.com>
2024-01-08 23:59:52 +0900

    fix: code

--- src/components/molecules/login/Login.tsx
+++ src/components/molecules/login/Login.tsx
@@ -4,7 +4,7 @@ import { KAKAO_LOGIN_URI } from '@utils/variables';
 function Login() {
 	console.log(process.env.NEXT_PUBLIC_REDIRECT_URI);
 	return (
-		<a className="loginBtn" href={KAKAO_LOGIN_URI}>
+		<a className="loginBtn" href={`${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI}`}>
 			<IconProfile />
 			<span>로그인</span>
 		</a>
