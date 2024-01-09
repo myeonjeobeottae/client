5c86f8e2280e72afd4a1a335cf94620245afa525
tangjinlog <lkg70007@gmail.com>
2024-01-09 18:15:28 +0900

    chore: 최신 코드 반영

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,5 +1,2 @@
 export const WANTED_URL = 'https://www.wanted.co.kr/wd';
-export const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-export const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
-export const KAKAO_LOGIN_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-console.log(REST_API_KEY);
+export const KAKAO_LOGIN_URI = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI;
