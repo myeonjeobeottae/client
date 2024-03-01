5cef11990acd2435cb264665455cd0ecc9da8c6b
tangjinlog <lkg70007@gmail.com>
2024-03-01 21:50:37 +0900

    fix: KAKO_LOGIN_URI 환경변수에서 받아오도록 수정

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,7 +1,5 @@
 export const WANTED_URL = 'https://www.wanted.co.kr/wd';
-export const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-export const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
-export const KAKAO_LOGIN_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_LOGIN_URI = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URI;
 
 // querykey
 export const queryKey = {
